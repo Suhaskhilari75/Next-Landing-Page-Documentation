a3e5f76388d9b7999d86d00eae9e6aeec7de9132
suhaskhilari75 <suhaskhilari75@gmail.com>
2024-10-03 15:57:50 +0530

    invoice search bar created

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,14 +1,31 @@
 'use client';
-
+import { useSearchParams ,usePathname,useRouter} from 'next/navigation';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import {useDebouncedCallback} from 'use-debounce'
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchparams=useSearchParams();
+  const pathname=usePathname();
+  const {replace}=useRouter();
+
+  const handleSearch=useDebouncedCallback((term:string)=>{
+    console.log(`Searching... ${term}`);
+
+    // URLSearchParams is a Web API that provides utility methods for manipulating the URL query parameters. 
+    // Instead of creating a complex string literal, you can use it to get the params string like
+    // ===> ?page=1&query=a.
+    const params=new URLSearchParams (searchparams);
+    term?params.set('query',term):params.delete('query')
+    replace(`${pathname}?${params.toString()}`)
+  },300)
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
       <input
+      defaultValue={searchparams.get('query')?.toString()}
+      onChange={(e)=>handleSearch(e.target.value)}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
       />
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+    // Next.js will prerender the static parts of your route and defer the dynamic parts until the user requests them.
+    experimental:{
+        ppr:'incremental'
+    }
+};
 
 export default nextConfig;

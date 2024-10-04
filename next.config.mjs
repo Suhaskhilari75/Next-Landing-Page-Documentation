6ae4bf13d20651329e86ac7b55cc2683c52803e5
suhaskhilari75 <suhaskhilari75@gmail.com>
2024-10-04 16:05:57 +0530

    lint:fix lint error

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,8 @@ const nextConfig = {
     // Next.js will prerender the static parts of your route and defer the dynamic parts until the user requests them.
     experimental:{
         ppr:'incremental'
-    }
+    },
+    eslint:false
 };
 
 export default nextConfig;
